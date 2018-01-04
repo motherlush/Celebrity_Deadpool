71d6e23440e4d02e3c245022ac9fb81a3fe6dfa2
Adam Strickland <adam.strickland@gmail.com>
2018-01-04 10:36:28 -0600

    adds devise omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
+  devise_scope :user do
+    get 'sign_in',     to: 'devise/sessions#new',     as: :new_user_session
+    get 'sign_out',    to: 'devise/sessions#destroy', as: :destroy_user_session
+    # delete 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
+  end
+
   get 'welcome/index'
+
   root to: 'welcome#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
